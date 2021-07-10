36399f3516d84d9cc2d2168b90365c08f62b4962
Carlito <carlorody1000@gmail.com>
2021-07-10 15:17:59 -0500

    app in progress

--- database/db.sql
+++ database/db.sql
@@ -2,6 +2,7 @@ CREATE DATABASE database_links;
 
 USE database_links;
 
+--users table
 CREATE TABLE users(
   id INT(11) NOT NULL,
   username VARCHAR(16) NOT NULL,
@@ -13,5 +14,23 @@ ALTER TABLE users
     ADD PRIMARY KEY (id):
 ALTER TABLE users
     MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
-    
+
 DESCRIBE users;
+
+-- links table
+CREATE TABLE links(
+  id INT(11) NOT NULL,
+  title VARCHAR(150) NOT NULL,
+  url VARCHAR(255) NOT NULL,
+  description TEXT,
+  user_id INT(11),
+  create_at timestamp NOT NULL DEFAULT current_timestamp
+  CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+ALTER TABLE links 
+    ADD PRIMARY KEY (id):
+
+ALTER table links
+    MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
+DESCRIBE links;
---
+++ src/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  database: {
+    host: "localhost",
+    user: "root",
+    password: "carlo20@",
+    database: "database_links",
+  },
+};
